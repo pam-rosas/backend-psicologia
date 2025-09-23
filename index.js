eae7183806b3db65af618649abb4b643cdd7d45e
matias-bello-rodriguez <materodriguez200011@gmail.com>
2025-09-23 10:33:06 -0300

    HORARIO-2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const port = 3000;
 const corsOptions = {
   origin: ['https://emhpsicoterapia.cl', 'http://localhost:4200'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
